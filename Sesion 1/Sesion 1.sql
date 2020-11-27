698dc38a3790db7b5ddb5c1da5da7ef758c8596e
AndresOrdMar3 <36243191+AndresOrdMar3@users.noreply.github.com>
2020-11-27 11:17:41 -0600

    Add files via upload

---
+++ Sesion 1/Sesion 1.sql
@@ -0,0 +1,10 @@
+USE tienda;
+
+SHOW TABLES;
+
+SELECT id_empleado, nombre FROM empleado WHERE id_puesto=4;
+SELECT id_puesto, nombre FROM puesto WHERE salario>10000;
+SELECT id_articulo, nombre FROM articulo WHERE precio>1000 AND iva>100;
+SELECT id_venta FROM venta WHERE id_articulo IN (135, 963) AND id_empleado IN (835, 369);
+
+SELECT nombre, salario FROM puesto ORDER BY salario DESC;
